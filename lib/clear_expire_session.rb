676886d5f79534b3427b11554c71b29dd1b4a8d7
Yuuki Tanaka <y.tanaka@everyleaf.com>
2010-04-28 19:18:39 +0900

    Railsの設定にあるタイムゾーンを見るように修正

--- lib/clear_expire_session.rb
+++ lib/clear_expire_session.rb
@@ -1,6 +1,7 @@
 class Session < ActiveRecord::Base
   def self.clear_expired(time = 60.minutes)
-    self.delete_all(["updated_at < ?", (Time.now - time)])
+    now = Time.zone ? Time.zone.now : Time.now
+    self.delete_all(["updated_at < ?", (now - time)])
   end
 end
 

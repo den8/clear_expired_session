bc37cb484d609be9b272955ad6d3ce30438dc902
Yuuki Tanaka <y.tanaka@everyleaf.com>
2010-04-28 15:14:06 +0900

    impliment clear logic

--- init.rb
+++ init.rb
@@ -1,1 +1,1 @@
-# Include hook code here
+require 'clear_expire_session'
--- lib/clear_expire_session.rb
+++ lib/clear_expire_session.rb
@@ -1,1 +1,6 @@
-# ClearExpireSession
+class Session < ActiveRecord::Base
+  def self.clear_expired(time = 60.minutes)
+    self.delete_all(["updated_at < ?", (Time.now - time)])
+  end
+end
+
